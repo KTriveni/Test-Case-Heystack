38e9ec32258d6330b08134337d044cb628c301c2
KTriveni <raghuraas@gmail.com>
2017-01-01 01:54:33 +0530

    sign-in signup pages

--- db/migrate/20161231174041_create_users.rb
+++ db/migrate/20161231174041_create_users.rb
@@ -40,6 +40,6 @@ class CreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    end
+    
   end
 end
